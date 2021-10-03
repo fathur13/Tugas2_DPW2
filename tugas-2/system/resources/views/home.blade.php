ad04fbfcfd1bf27c45ca841a630c05e311a97dcd
fikrizulfikarb <91817157+fathur13@users.noreply.github.com>
2021-10-03 21:14:18 +0700

    tugas 2

---
+++ tugas-2/system/resources/views/home.blade.php
@@ -0,0 +1,208 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Lesy</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="freehtml5.co" />
+
+	<!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FreeHTML5.co
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="{{url('public')}}/https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,300i,400,400i,500,600i,700" rel="stylesheet">
+	<link href="{{url('public')}}/https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+	
+	<!-- Animate.css -->
+                <link rel="stylesheet" href="{{url('public')}}/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+                <link rel="stylesheet" href="{{url('public')}}/css/icomoon.css">
+	<!-- Bootstrap  -->
+                <link rel="stylesheet" href="{{url('public')}}/css/bootstrap.css">
+	<!-- Flexslider  -->
+                <link rel="stylesheet" href="{{url('public')}}/css/flexslider.css">
+	<!-- Theme style  -->
+                <link rel="stylesheet" href="{{url('public')}}/css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="{{url('public')}}/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<!-- <div class="top-menu"> -->
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 text-center logo-wrap">
+						<div id="fh5co-logo"><a href="index.html">Lesy<span>.</span></a></div>
+					</div>
+					<div class="col-xs-12 text-center menu-1 menu-wrap">
+						<ul>
+							<li class="active"><a href="index.html">Home</a></li>
+							<li><a href="{{url('public')}}/menu.html">Menu</a></li>
+							<li><a href="{{url('public')}}/contact.html">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		<!-- </div> -->
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover js-fullheight" role="banner" style="{{url('public')}}/img(images/hero_1.jpeg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<div class="display-t js-fullheight">
+						<div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
+							<h1>The Best Coffee <em>&amp;</em> Restaurant <em>in</em> Lesy</h1>
+							<h2>Brought to you by Fathur Rahman </h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-about" class="fh5co-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-pull-4 img-wrap animate-box" data-animate-effect="fadeInLeft">
+					<img src="{{url('public')}}/images/hero_1.jpeg" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+				</div>
+				<div class="col-md-5 col-md-push-1 animate-box">
+					<div class="section-heading">
+						<h2>The Restaurant</h2>
+						<p>Didirikan tahun 2021, Lesy adalah restoran modern dengan desain interior stylish. Lesy ini memiliki beragam desain setting ruangan, seperti area jamuan makan di lantai satu gedung utama, 11 tatami (tikar dengan meja untuk 10 orang) di lantai dua, dan 12 Gazebo di halaman belakang.
+
+							</p>
+						<p></p>
+						<p><a href="#" class="btn btn-primary btn-outline">Our History</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-featured-menu" class="fh5co-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 fh5co-heading animate-box">
+					<h2>Today's Menu</h2>
+					<div class="row">
+						<div class="col-md-6">
+							<p>Restoran ini menyediakan hidangan khas Indonesia, hidangan laut, dan hidangan Barat. Spesialisasi makanan mereka adalah Baka Potato Pizza, Salted Fried Chicken, Italian Sauce Mushroom, Fried Potato w/ Garlic, dan lainnya. Mereka juga menyediakan minuman beralkohol seperti anggur, sampanye, cognan, dan lainnya.</p>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+					<div class="fh5co-item">
+						<img src="{{url('public')}}/images/gallery_9.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<h3>Bake Potato Pizza</h3>
+						<span class="fh5co-price">Rp,286.100<sup>.00</sup></span>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+					<div class="fh5co-item margin_top">
+						<img src="{{url('public')}}/images/gallery_8.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<h3>Salted Fried Chicken</h3>
+						<span class="fh5co-price">Rp.271.795<sup>.00</sup></span>
+					</div>
+				</div>
+				<div class="clearfix visible-sm-block visible-xs-block"></div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+					<div class="fh5co-item">
+						<img src="{{url('public')}}/images/gallery_7.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<h3>Italian Sauce Mushroom</h3>
+						<span class="fh5co-price">Rp,243.185<sup>.00</sup></span>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap animate-box">
+					<div class="fh5co-item margin_top">
+						<img src="{{url('public')}}/images/gallery_6.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<h3>Fried Potato w/ Garlic</h3>
+						<span class="fh5co-price">Rp,314.710<sup>.00</sup></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer" role="contentinfo" class="fh5co-section">
+		<div class="container">
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2021 Restaurant Lesy.</small> 
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter2"></i></a></li>
+							<li><a href="#"><i class="icon-facebook2"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin2"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble2"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="{{url('public')}}/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="{{url('public')}}/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="{{url('public')}}/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="{{url('public')}}/js/jquery.waypoints.min.js"></script>
+	<!-- Waypoints -->
+	<script src="{{url('public')}}/js/jquery.stellar.min.js"></script>
+	<!-- Flexslider -->
+	<script src="{{url('public')}}/js/jquery.flexslider-min.js"></script>
+	<!-- Main -->
+	<script src="{{url('public')}}/js/main.js"></script>
+
+	</body>
+</html>
+
